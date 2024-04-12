3fafe97faf2f7f8beb17254b8f66d5c09716dd05
Ranjeet Barsa <ranjeet.barsa@brainvire.com>
2024-04-12 13:15:59 +0530

    feat: gnews integration

--- src/App.js
+++ src/App.js
@@ -1,55 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios  from 'axios';
 
-import CourseGoalList from './components/CourseGoals/CourseGoalList/CourseGoalList';
-import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 import './App.css';
+import { Header } from './components/UI/Header/Header';
+import News from './components/UI/News/News';
+
+const fetchData = async() =>{
+  const data = await axios.get("https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=in&max=10&apikey=0768291cf49b62749cf771dd344ba952");
+  return data;
+}
 
 const App = () => {
-  const [courseGoals, setCourseGoals] = useState([
-    { text: 'Do all exercises!', id: 'g1' },
-    { text: 'Finish the course!', id: 'g2' }
-  ]);
 
-  const addGoalHandler = enteredText => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = [...prevGoals];
-      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
-      return updatedGoals;
-    });
-  };
+  const [news, setNews] = useState([]);
 
-  const deleteItemHandler = goalId => {
-    setCourseGoals(prevGoals => {
-      const updatedGoals = prevGoals.filter(goal => goal.id !== goalId);
-      return updatedGoals;
-    });
-  };
-
-  let content = (
-    <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-  );
-
-  if (courseGoals.length > 0) {
-    content = (
-      <CourseGoalList items={courseGoals} onDeleteItem={deleteItemHandler} />
-    );
-  }
+  useEffect(()=>{
+    (async()=>{
+      const {data} = await fetchData();
+      setNews(data.articles);
+    })();
+  }, []);
 
   return (
     <div>
-      <section id="goal-form">
-        <CourseInput onAddGoal={addGoalHandler} />
-      </section>
-      <section id="goals">
-        {content}
-        {/* {courseGoals.length > 0 && (
-          <CourseGoalList
-            items={courseGoals}
-            onDeleteItem={deleteItemHandler}
-          />
-        ) // <p style={{ textAlign: 'center' }}>No goals found. Maybe add one?</p>
-        } */}
-      </section>
+      <Header/>
+      {news.map((articles)=><News articles={articles}/>)}
     </div>
   );
 };
