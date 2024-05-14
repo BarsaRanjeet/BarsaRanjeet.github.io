d1c21021905e2a702a4b632c38a138132bba811e
Ranjeet Barsa <ranjeet.barsa@brainvire.com>
2024-05-14 19:28:44 +0530

    feat: cleanup

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import axios  from 'axios';
+import React from 'react';
 
 import './App.css';
-import { Header } from './components/UI/Header/Header';
-import News from './components/UI/News/News';
-
-const fetchData = async() =>{
-  const data = await axios.get("https://gnews.io/api/v4/top-headlines?category=general&lang=en&country=in&max=10&apikey=");
-  return data;
-}
 
 const App = () => {
-
-  const [news, setNews] = useState([]);
-
-  useEffect(()=>{
-    (async()=>{
-      const {data} = await fetchData();
-      setNews(data.articles);
-    })();
-  }, []);
-
-  return (
-    <div>
-      <Header/>
-      {news.map((articles)=><News articles={articles}/>)}
-    </div>
-  );
+  return <>
+    <h1>Hello</h1>
+  </>
 };
 
 export default App;
