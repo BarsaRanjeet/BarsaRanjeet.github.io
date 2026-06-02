bbaa50da7ad4eb695c6741badf79ce248dd346ec
Ranjeet Barsa <barsaranjeet15@gmail.com>
2026-06-02 12:05:07 +0530

    feat: made changes for suggested solution

--- src/store.js
+++ src/store.js
@@ -1,18 +1,16 @@
-import { legacy_createStore as createStore } from 'redux'
+import { legacy_createStore as createStore } from "redux";
 
 const initialState = {
   sidebarShow: true,
-  theme: 'light',
-}
+  theme: "light",
+};
 
 const changeState = (state = initialState, { type, ...rest }) => {
-  switch (type) {
-    case 'set':
-      return { ...state, ...rest }
-    default:
-      return state
+  if (type === "set") {
+    return { ...state, ...rest };
   }
-}
+  return state;
+};
 
-const store = createStore(changeState)
-export default store
+const store = createStore(changeState);
+export default store;
